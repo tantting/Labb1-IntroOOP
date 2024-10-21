a5a5a13771f4daccd6e30a07a286e3ee9f665cea
tantting <jennyann.hayward82@gmail.com>
2024-10-21 08:27:31 +0200

    Added method for calc circumference, volume sphere and print out result

--- Program.cs
+++ Program.cs
@@ -6,10 +6,9 @@ class Program
     {
         Circle circle1 = new Circle(5);
         Circle circle2 = new Circle(6);
-
-        Console.WriteLine($"Arean av cirklarna är: " +
-                          $"Radie {circle1.Radius} ger area {circle1.Area():N}\n" +
-                          $"Radie {circle2.Radius} ger area {circle2.Area():N}\n" +
-                          $"Enhet för radie och area är oklart :)" );
+        
+        circle1.Print();
+        Console.WriteLine();
+        circle2.Print();
     }
 }
